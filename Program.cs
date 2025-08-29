6dd1fea013e6c04d82d9fbec41d0102e3af07a96
kfje <kfje@itu.dk>
2025-08-29 07:53:20 +0200

    refactored Program.cs to print CLI args
    Co-authored by Andreas

--- Program.cs
+++ Program.cs
@@ -1,9 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-if (args[0] == "say")
-{
-    var message = args[1];
-    var frequency = int.Parse(args[2]);
-    foreach (var i in Enumerable.Range(1, frequency))
-        Console.Write(message + " ");
-}
+﻿foreach (var arg in args)
+    Console.WriteLine(arg);
