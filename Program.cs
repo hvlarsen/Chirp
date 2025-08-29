a5502c5e4e66532ca93285843f5afac8e6f2091b
kfje <kfje@itu.dk>
2025-08-29 07:51:06 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+if (args[0] == "say")
+{
+    var message = args[1];
+    var frequency = int.Parse(args[2]);
+    foreach (var i in Enumerable.Range(1, frequency))
+        Console.Write(message + " ");
+}
