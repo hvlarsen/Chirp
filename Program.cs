28a423baf5ed49d3fcbe45e303d1a1c4f0f0e8ca
Andreas Lindsted <anlin@itu.dk>
2025-08-29 11:07:39 +0200

    Attempting to write to CSV file
    Trying to write to the csv file using the CSVHelper library

--- Program.cs
+++ Program.cs
@@ -6,16 +6,27 @@ using System.Collections.Generic;
 
 public class Program
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        using (var reader = new StreamReader("chirp_cli_db.csv"))
-        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-        {
-            var messages = csv.GetRecords<ChirpOutput>();
-            foreach (var message in messages)
-            {
+        using var reader = new StreamReader("chirp_cli_db.csv");
+        using var csv1 = new CsvReader(reader, CultureInfo.InvariantCulture);
+        using var writer = new StreamWriter("chirp_cli_db.csv");
+        using var csv2 = new CsvWriter(writer, CultureInfo.InvariantCulture);
+        if (args[0] == "read")
+        { 
+            var messagesOut = csv1.GetRecords<ChirpOutput>(); 
+            foreach (var message in messagesOut)
+            { 
                 Console.WriteLine($"{message.Author}: {message.Message} ({message.Timestamp})");
             }
+        }
+        else if (args[0] == "cheep")
+        {
+            var messagesIn = new List<Cheep>
+            {
+                new Cheep { Author = "TestName", Message = args[1], Timestamp = 12 }
+            };
+            csv2.WriteRecords(messagesIn);
         }
     }
 }
@@ -24,7 +35,14 @@ public class Program
 
 public class ChirpOutput
 {
-    public string Author { get; set; }
-    public string Message { get; set; }
+    public required string Author { get; set; }
+    public required string Message { get; set; }
+    public int Timestamp { get; set; }
+}
+
+public class Cheep
+{    
+    public required string Author { get; set; }
+    public required string Message { get; set; }
     public int Timestamp { get; set; }
 }
