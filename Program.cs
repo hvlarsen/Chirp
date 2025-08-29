ce7b18216182a2ff235fd2d60aeb022620ca70b4
Andreas Lindsted <anlin@itu.dk>
2025-08-29 10:37:38 +0200

    Reads and outputs CSV data
    Using CSVHelper, Program.cs now outputs the messages.
    
    Timestamp conversion is missing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿foreach (var arg in args)
-    Console.WriteLine(arg);
+﻿using System;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        using (var reader = new StreamReader("chirp_cli_db.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            var messages = csv.GetRecords<ChirpOutput>();
+            foreach (var message in messages)
+            {
+                Console.WriteLine($"{message.Author}: {message.Message} ({message.Timestamp})");
+            }
+        }
+    }
+}
+
+
+
+public class ChirpOutput
+{
+    public string Author { get; set; }
+    public string Message { get; set; }
+    public int Timestamp { get; set; }
+}
