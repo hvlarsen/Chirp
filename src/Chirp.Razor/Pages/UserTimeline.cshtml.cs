ab8d3b62a0201898e398f1f29167733a7cc6a4dc
Hans Larsen <havl@itu.dk>
2025-09-25 21:14:32 +0200

    Add Chirp.Razor ASP.NET Razor Pages project
    Introduces a new Chirp.Razor web project with Razor Pages for public and user timelines, a CheepService for data access, layout and styling, and required configuration files. Updates the solution to include the new project and adds support for multiple build configurations.

---
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Chirp.SimpleDB; // Only because it needs to know Cheep.cs
+
+namespace Chirp.Razor.Pages;
+
+public class UserTimelineModel : PageModel
+{
+    private readonly ICheepService _service;
+    public List<Cheep> Cheeps { get; set; }
+
+    public UserTimelineModel(ICheepService service)
+    {
+        _service = service;
+    }
+
+    public ActionResult OnGet(string author)
+    {
+        Cheeps = _service.GetCheepsFromAuthor(author);
+        return Page();
+    }
+}
