4f58a4988f62a94e96d7e25267c4f359292a5830
Hans Larsen <havl@itu.dk>
2025-09-26 13:34:16 +0200

    Add Chirp.Razor test project and basic API tests
    Introduces the Chirp.Razor.Tests project with dependencies for xUnit and ASP.NET Core test utilities. Adds initial tests for public and private timelines, and updates Program.cs to support WebApplicationFactory for integration testing.

---
+++ test/Chirp.Razor.Tests/TestApi.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Chirp.Razor.Tests;
+
+public class TestAPI : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _fixture;
+    private readonly HttpClient _client;
+
+    public TestAPI(WebApplicationFactory<Program> fixture)
+    {
+        // Copy csv test file 
+        var baseDir = AppContext.BaseDirectory;
+        var targetPath = Path.Combine(baseDir, "chirp_cli_db.csv");
+        var sourcePath = Path.Combine("..", "..", "..", "..", "..", "data", "chirp_cli_db_test.csv");
+
+        File.Copy(sourcePath, targetPath, overwrite: true);
+        
+        _fixture = fixture;
+        _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
+    }
+
+    [Fact]
+    public async Task CanSeePublicTimeline()
+    {
+        var response = await _client.GetAsync("/");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains("Public Timeline", content);
+    }
+
+    [Theory]
+    [InlineData("alexm")]
+    [InlineData("adho")]
+    public async Task CanSeePrivateTimeline(string author)
+    {
+        var response = await _client.GetAsync($"/{author}");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains($"{author}'s Timeline", content);
+    }
+}
